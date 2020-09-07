3cc0420f19f51138f03e86319042e854ae55d049
robintroyer <62710623+robintroyer@users.noreply.github.com>
2020-09-07 10:06:45 +0200

    table created

--- models/Classes/Storage/Database.php
+++ models/Classes/Storage/Database.php
@@ -6,16 +6,20 @@ class Database implements StorageInterface
     public function initialize($config)
     {
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-        $this->conn = new mysqli($config->server, $config->username, $config->password);
+        $this->conn = new mysqli($config->server, $config->username, $config->password,
+        $config->database);
         if ($this->conn->connect_error) {
             die('Connection failed: ' . $this->conn->connect_error);
         }
 
-        $sql = "CREATE DATABASE gallery";
+        $sql = "CREATE TABLE galleries(
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(50) NOT NULL
+        )";
         if ($this->conn->query($sql)) {
-            echo 'Database created successfully';
+            echo 'Table galleries created successfully';
         } else {
-            echo 'Error creating database: ' . $this->conn->error;
+            echo 'Error creating table ' . $this->conn->error;
         }
     }
     public function saveEntry($data)
